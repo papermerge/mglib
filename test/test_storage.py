e358e748bdea4e9747e31cb5a915dbb6754371bf
Eugen Ciur <eugen@django-lessons.com>
2020-05-04 16:09:15 +0200

    tests for storage delete function

--- test/test_storage.py
+++ test/test_storage.py
@@ -1,16 +1,43 @@
+import os
 import unittest
+from mglib.test.utils import TemporaryDir
 from mglib.path import DocumentPath
 from mglib.storage import Storage
 
 
-class TestStep(unittest.TestCase):
+BASE_DIR = os.path.dirname(
+    os.path.abspath(__file__)
+)
 
-    def test_basic(self):
-        storage = Storage()
+MEDIA_ROOT = os.path.join(
+    BASE_DIR, "media"
+)
 
-        docp = DocumentPath(
-            user_id=1,
-            document_id=2,
-            file_name='doku.pdf'
-        )
-        storage.delete_document(docp)
+class TestStorage(unittest.TestCase):
+
+    def test_delete(self):
+        storage = Storage(location=MEDIA_ROOT)
+
+        with TemporaryDir(MEDIA_ROOT) as media_root:
+            docs = media_root.add_folder("docs")
+            res = media_root.add_folder("results")
+            f1 = docs.add_folder("user_1/document_2")
+            f1.add_file("doku.pdf")
+            res.add_folder("user_1/document_2/pages")
+
+            docp = DocumentPath(
+                user_id=1,
+                document_id=2,
+                file_name='doku.pdf'
+            )
+
+            storage.delete_document(docp)
+
+            self.assertFalse(
+                docs.exists()
+            )
+
+            self.assertFalse(
+                res.exists()
+            )
+
