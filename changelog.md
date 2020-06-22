6759e3506081f02489324dc06614dfae6fb22925
Eugen Ciur <eugen@papermerge.com>
2020-06-22 08:35:39 +0200

    added load_config and try_load_config functions

--- changelog.md
+++ changelog.md
@@ -3,7 +3,8 @@
 ## [1.1.0] - work in progress
 
 ### Added 
-   
+    - utils.try_load_config
+    - utils.load_config
     - Endpoint module move in (from pmworker)
 
 ## [1.0.0] - 25 Apr 2020
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mglib",
-    version="1.1.0",
+    version="1.1.1",
     author="Eugen Ciur",
     author_email="eugen@papermerge.com",
     url="https://github.com/papermerge/mglib",
